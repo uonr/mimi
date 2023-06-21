444b7176f3b1cfe563a039a5d964a6f40a3acdb5
uonr <34709816+uonr@users.noreply.github.com>
2023-06-21 21:56:14 +0900

    feat: use trap

--- scripts/update-secrets.sh
+++ scripts/update-secrets.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-SIGNATURE_FILE=$(mktemp)
-FETCHED_FILE=$(mktemp)
+SIGNATURE_FILE=$(mktemp) || exit 1
+FETCHED_FILE=$(mktemp) || exit 1
+
+# Cleanup when the script exits
+# http://redsymbol.net/articles/bash-exit-traps/
+function cleanup () {
+  rm "$SIGNATURE_FILE" "$FETCHED_FILE" 
+}
+trap finish EXIT
 
 curl -s -F "key=@${HOST_PUBLIC_KEY_PATH:?}" -X POST "https://${MIMI_DOMAIN:?}/sign/${NODE_ID:?}" -o "$SIGNATURE_FILE"
 curl -s -F "key=@${HOST_PUBLIC_KEY_PATH:?}" -X POST "https://${MIMI_DOMAIN:?}/get/${NODE_ID:?}" | rage --decrypt -i "${HOST_KEY_PATH:?}" -o "$FETCHED_FILE"
@@ -9,10 +16,8 @@ if ssh-keygen -Y check-novalidate -n file -f "${PROVISIONER_PUBLIC_KEY:?}" -s "$
   echo "Signature is valid."
 else
   echo "Signature is invalid."
-  rm "$SIGNATURE_FILE" "$FETCHED_FILE"
   exit 1
 fi
 mkdir -p /etc/mimi-secrets/
 chmod 700 /etc/mimi-secrets/
 tar -xzf "$FETCHED_FILE" -C /etc/mimi-secrets/ --overwrite-dir --recursive-unlink --mode "o=rwx"
-rm "$SIGNATURE_FILE" "$FETCHED_FILE"
